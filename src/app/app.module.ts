b12f9805b8a4d216046c8564c8d343241d2cf217
simonayo <ayosimon.dev@gmail.com>
2024-02-12 13:37:36 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +23,9 @@ import { SidebarComponent } from './layouts/full/sidebar/sidebar.component';
 import { HeaderComponent } from './layouts/full/header/header.component';
 import { BrandingComponent } from './layouts/full/sidebar/branding.component';
 import { AppNavItemComponent } from './layouts/full/sidebar/nav-item/nav-item.component';
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
+import { IHttpInterceptor } from './core/services/interceptor';
+import { NotifierModule } from 'angular-notifier';
 
 @NgModule({
   declarations: [
@@ -43,6 +46,24 @@ import { AppNavItemComponent } from './layouts/full/sidebar/nav-item/nav-item.co
     ReactiveFormsModule,
     MaterialModule,
     TablerIconsModule.pick(TablerIcons),
+    NgxUiLoaderModule,
+    NotifierModule.withConfig({
+      position: {
+        horizontal: {
+          position: 'right',
+        },
+        vertical: {
+          position: 'top',
+        },
+      },
+    }),
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: IHttpInterceptor,
+      multi: true,
+    },
   ],
   exports: [TablerIconsModule],
   bootstrap: [AppComponent],
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,11 +1,97 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthService {
-  baseUrl = environment.baseUrl
-  constructor() { }
-  
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { ILogin, UserInfoType } from '../model/auth.model';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+  private baseUrl = environment.baseUrl;
+
+  private jwtHelper: JwtHelperService = new JwtHelperService();
+
+  public loggedInUser$: BehaviorSubject<any> = new BehaviorSubject({});
+
+  constructor(private httpClient: HttpClient, private router: Router) {}
+
+  private get getToken(): string | null {
+    return localStorage['token']?.replaceAll('"', '');
+  }
+
+  private get getRefreshToken(): string | null {
+    return localStorage['refresh']?.replaceAll('"', '');
+  }
+
+  public get getUserInfo(): any | null {
+    const userInfo: any | null = localStorage['userInfo'];
+    return userInfo && JSON.parse(userInfo);
+  }
+
+  private setUserInfo(userInfo: UserInfoType): void {
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+  }
+
+  public setToken(token: string): void {
+    localStorage.setItem('token', JSON.stringify(token));
+  }
+
+  private setRefreshToken(refresh: string): void {
+    localStorage.setItem('refresh', JSON.stringify(refresh));
+  }
+
+  private clearStorage(): void {
+    localStorage.clear();
+  }
+
+  public login(payload: ILogin, userType: 'admin' | 'user'): Observable<any> {
+    return this.httpClient
+      .post(`${this.baseUrl}/${userType}-login`, payload)
+      .pipe(
+        tap((result: any) => {
+          this.clearStorage();
+          const { success } = result;
+          if (success) {
+            const userInfo: UserInfoType = {
+              username: payload.username,
+              firstName: '',
+              lastName: '',
+              email: '',
+            };
+
+            this.setUserInfo(userInfo);
+            this.setToken(result.token);
+            //   this.setRefreshToken(data.refreshToken)
+          }
+        })
+      );
+  }
+
+  public refreshToken(): Observable<any> {
+    return this.httpClient
+      .post<any>(`${this.baseUrl}/refresh-token`, {
+        token: this.getRefreshToken,
+      })
+      .pipe(
+        tap((result: any) => {
+          console.log('Refreshed Token: ', result);
+          this.setToken(result.accessToken);
+          this.setRefreshToken(result.refreshToken);
+        })
+      );
+  }
+
+  public isAuthenticated() {
+    const token: string | null = this.getToken;
+    if (token && !this.jwtHelper.isTokenExpired(token)) return true;
+    return false;
+  }
+
+  public logout() {
+    this.router.navigate(['/']);
+    localStorage.clear();
+  }
 }
