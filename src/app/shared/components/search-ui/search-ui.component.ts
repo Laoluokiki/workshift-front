01db59d59c24eeb34d88ecdf5fede9423371e677
simonayo <ayosimon.dev@gmail.com>
2024-02-14 14:03:35 +0100

    update

---
+++ src/app/shared/components/search-ui/search-ui.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { debounceTime, distinctUntilChanged, filter, takeUntil } from "rxjs";
+import { Unsubscribe } from "src/app/core/services/unsubscribe.class";
+
+@Component({
+  selector: "app-search-ui",
+  templateUrl: "./search-ui.component.html",
+  styleUrls: ["./search-ui.component.scss"],
+})
+export class SearchUiComponent extends Unsubscribe implements OnInit {
+  public search: FormControl = new FormControl();
+  @Input() showSearchInput: boolean = true;
+  @Input() placeholder: string = "Search...";
+  @Output() onSearch: EventEmitter<string> = new EventEmitter();
+
+  constructor() {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.search.valueChanges
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(800),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe({
+        next: (searchVal) => this.onSearch.emit(searchVal),
+      });
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar/calendar.component';
-
-
+import { PageTitlesComponent } from './components/page-titles/page-titles.component';
+import { TableUiComponent } from './components/table/table.component';
+import { SearchUiComponent } from './components/search-ui/search-ui.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
   declarations: [
-    CalendarComponent
+    CalendarComponent,
+    CalendarComponent,
+    PageTitlesComponent,
+    TableUiComponent,
+    SearchUiComponent,
+    PaginatorComponent,
   ],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   exports: [
-    CalendarComponent
-  ]
+    CalendarComponent,
+    PageTitlesComponent,
+    TableUiComponent,
+    SearchUiComponent,
+    PaginatorComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
