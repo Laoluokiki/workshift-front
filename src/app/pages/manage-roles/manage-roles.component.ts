815cb20b5085f0fb75e32297a698ce14221e4c32
dharnzkraft <adeyinkadaniel09@gmail.com>
2024-02-14 23:41:46 +0100

    update

--- src/app/pages/manage-roles/manage-roles.component.ts
+++ src/app/pages/manage-roles/manage-roles.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IPagination, ITableColumn } from 'src/app/core/model/table.model';
 import { AdminService } from 'src/app/core/services/admin.service';
 import { NotificationService } from 'src/app/core/services/notification.service';
 
@@ -9,15 +10,34 @@ import { NotificationService } from 'src/app/core/services/notification.service'
 })
 export class ManageRolesComponent {
   roles: any; 
+  roleState: "roleTable" | "addRole" | "updateRole" = "roleTable"
   constructor(
     private adminService: AdminService,
     private notifier: NotificationService
   ){
-
+    this.getAllRoles()
   }
   getAllRoles(){
     this.adminService.getRoles().subscribe((response: any)=>{
       this.roles = response
+      console.log(response)
     })
   }
+
+  public roleColumns: ITableColumn[] = [
+    {
+      name: "Role",
+      type: "text",
+      key: "name_of_department"
+    },
+    
+  ];
+
+  public pagination: IPagination = {
+    next: "",
+    previous: "",
+    hasNext: false,
+    hasPrevious: false,
+    total: 0,
+  };
 }
