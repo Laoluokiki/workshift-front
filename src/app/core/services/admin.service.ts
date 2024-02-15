6ceea991864819f606917a91ea31ecb8b7de269c
simonayo <ayosimon.dev@gmail.com>
2024-02-15 14:06:20 +0100

    update

--- src/app/core/services/admin.service.ts
+++ src/app/core/services/admin.service.ts
@@ -4,53 +4,56 @@ import { environment } from 'src/environments/environment';
 import { ICreateAdmin, ICreateRole } from '../model/userRequest.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminService {
-
   private baseUrl = environment.baseUrl;
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
-  public getAllUser(){
-    return this.httpClient.get(this.baseUrl+`/user`)
+  public getAllUser() {
+    return this.httpClient.get(this.baseUrl + `/user`);
+  }
+
+  public getAllShifts() {
+    return this.httpClient.get(this.baseUrl + `/Users-shift`);
+  }
+  public getAllUsersRoles() {
+    return this.httpClient.get(this.baseUrl + `/userroles`);
   }
 
   public getAllDept() {
-    return this.httpClient.get(this.baseUrl+`/departments`)
+    return this.httpClient.get(this.baseUrl + `/departments`);
   }
 
-  public createDept(payload: any){
-    return this.httpClient.post(this.baseUrl+`/create-dept`, payload)
+  public createDept(payload: any) {
+    return this.httpClient.post(this.baseUrl + `/create-dept`, payload);
   }
 
-  public updateDept(payload: any){
-    return this.httpClient.put(this.httpClient+`/create-dept`, payload)
+  public updateDept(payload: any) {
+    return this.httpClient.patch(this.httpClient + `/create-dept`, payload);
   }
 
-  public createAdmin(payload: ICreateAdmin){
-    return this.httpClient.post(this.baseUrl+`/admin-user`, payload)
+  public createAdmin(payload: ICreateAdmin) {
+    return this.httpClient.post(this.baseUrl + `/admin-user`, payload);
   }
 
-  public getAllAdmin(){
-    return this.httpClient.get(this.baseUrl+`/admin`)
+  public getAllAdmin() {
+    return this.httpClient.get(this.baseUrl + `/admin`);
   }
 
-  public updateAdmin(payload: any, username: any){
-    return this.httpClient.put(this.baseUrl+`/update-admin/${username}`, payload)
+  public updateAdmin(payload: any) {
+    return this.httpClient.patch(this.baseUrl + ``, payload);
   }
 
-  public createRoles(payload: ICreateRole){
-    return this.httpClient.post(this.baseUrl+`/create-roles`, payload)
+  public createRoles(payload: any) {
+    return this.httpClient.post(this.baseUrl + `/create-roles`, payload);
   }
 
-  public updateRoles(payload: any, roleId: any){
-    return this.httpClient.put(this.baseUrl+`/update-roles?role_id=${roleId}`, payload)
+  public updateRoles(payload: any) {
+    return this.httpClient.patch(this.baseUrl + `/create-roles`, payload);
   }
 
-  public getRoles(){
-    return this.httpClient.get(this.baseUrl+`/roles`)
+  public getRoles() {
+    return this.httpClient.get(this.baseUrl + `/roles`);
   }
-
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,22 +9,29 @@ import { NgApexchartsModule } from 'ng-apexcharts';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import * as TablerIcons from 'angular-tabler-icons/icons';
 import { AppDashboardComponent } from './dashboard/dashboard.component';
-import { SharedModule } from "../shared/shared.module";
+import { SharedModule } from '../shared/shared.module';
 import { ManageAdminsComponent } from './manage-admins/manage-admins.component';
 import { ManageDepartmentsComponent } from './manage-departments/manage-departments.component';
 import { ManageRolesComponent } from './manage-roles/manage-roles.component';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
 
 @NgModule({
-    declarations: [AppDashboardComponent, ManageAdminsComponent, ManageDepartmentsComponent, ManageRolesComponent],
-    exports: [TablerIconsModule],
-    imports: [
-        CommonModule,
-        MaterialModule,
-        FormsModule,
-        NgApexchartsModule,
-        RouterModule.forChild(PagesRoutes),
-        TablerIconsModule.pick(TablerIcons),
-        SharedModule
-    ]
+  declarations: [
+    AppDashboardComponent,
+    ManageAdminsComponent,
+    ManageDepartmentsComponent,
+    ManageRolesComponent,
+    ManageUsersComponent,
+  ],
+  exports: [TablerIconsModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    NgApexchartsModule,
+    RouterModule.forChild(PagesRoutes),
+    TablerIconsModule.pick(TablerIcons),
+    SharedModule,
+  ],
 })
 export class PagesModule {}
